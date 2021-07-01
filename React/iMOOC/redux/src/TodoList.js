ae4fbcf522e27c38b3832c934b81586525181a38
suvanlly <swl381029812@gmail.com>
2021-07-01 12:28:33 +1000

    modify redux

---
+++ React/iMOOC/redux/src/TodoList.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button } from 'antd';
+
+class TodoList extends Component {
+
+  render() {
+    return (
+      <div style={{marginTop: '10px', marginLeft: '10px'}}>
+        <div>
+          <Input placeholder='todo info' style={{width: '300px', marginRight: '10px'}}/>
+          <Button type="primary">Submit</Button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TodoList;
