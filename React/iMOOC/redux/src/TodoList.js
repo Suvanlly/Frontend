8547d86bf02b674bb7d78259ae61fd17ed9cf131
suvanlly <swl381029812@gmail.com>
2021-07-16 16:46:01 +1000

    create list under redux

--- React/iMOOC/redux/src/TodoList.js
+++ React/iMOOC/redux/src/TodoList.js
@@ -1,7 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React, {Component} from 'react';
 import 'antd/dist/antd.css';
-import { Input, Button } from 'antd';
+import { Input, Button, List } from 'antd';
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
 
 class TodoList extends Component {
 
@@ -12,6 +20,12 @@ class TodoList extends Component {
           <Input placeholder='todo info' style={{width: '300px', marginRight: '10px'}}/>
           <Button type="primary">Submit</Button>
         </div>
+        <List
+          style={{marginTop: '10px', width: '300px'}}
+          bordered
+          dataSource={data}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
       </div>
     )
   }
