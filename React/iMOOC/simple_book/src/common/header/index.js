47ee365196e252c6caeb38dfffabff3835af35fc
suvanlly <swl381029812@gmail.com>
2021-08-15 16:00:49 +1000

    search box

--- React/iMOOC/simple_book/src/common/header/index.js
+++ React/iMOOC/simple_book/src/common/header/index.js
@@ -6,10 +6,21 @@ import {
   NavItem,
   NavSearch,
   Addition,
-  Button
+  Button,
+  SearchWrapper
 } from './style'
 
 class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: false
+    }
+    this.handleInputFocus = this.handleInputFocus.bind(this)
+    this.handleInputBlur = this.handleInputBlur.bind(this)
+  }
+
   render() { 
     return (
       <HeaderWrapper>
@@ -18,15 +29,39 @@ class Header extends Component {
           <NavItem className='left active'>Home</NavItem>
           <NavItem className='left'>Download</NavItem>
           <NavItem className='right'>Login</NavItem>
-          <NavItem className='right'>Aa</NavItem>
-          <NavSearch></NavSearch>
+          <NavItem className='right'>
+            <i className="iconfont">&#xe939;</i>
+          </NavItem>
+          <SearchWrapper>
+            <NavSearch 
+              // 如果现在是focus状态，就让NavSearch增加一个class叫’focused’， 否则就什么class都没有；因为focused初始值是false所以目前什么class都没有
+              className={this.state.focused ? 'focused': ''} 
+              onFocus={this.handleInputFocus}
+              onBlur={this.handleInputBlur}
+            ></NavSearch>
+            <i className={this.state.focused ? 'focused iconfont': 'iconfont'}>&#xe612;</i>
+          </SearchWrapper>
         </Nav>
         <Addition>
+          <Button>
+            <i className="iconfont">&#xe708;</i>
+            <span>Write</span>
+          </Button>
           <Button>Register</Button>
-          <Button>Write</Button>
         </Addition>
       </HeaderWrapper>
     );
+  }
+
+  handleInputFocus() {
+    this.setState({
+      focused: true
+    })
+  }
+  handleInputBlur() {
+    this.setState({
+      focused: false
+    })
   }
 }
  
--- React/iMOOC/simple_book/src/common/header/style.js
+++ React/iMOOC/simple_book/src/common/header/style.js
@@ -45,12 +45,31 @@ export const NavItem = styled.div`
   }
 `
 
+export const SearchWrapper = styled.div`
+  position: relative;
+  float: left;
+  .iconfont {
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+    width: 30px;
+    line-height: 30px;
+    text-align: center;
+    border-radius: 15px;
+    background: #eee;
+    &.focused {
+      background: #999;
+      color: #fff;
+    }
+  }
+`
+
 export const NavSearch = styled.input.attrs({
   placeholder: 'Search'
 })`
   width: 160px;
   height: 38px;
-  padding: 0 20px;
+  padding: 0 30px 0 20px;
   margin-top: 10px;
   margin-left: 20px;
   box-sizing: border-box;
@@ -59,8 +78,12 @@ export const NavSearch = styled.input.attrs({
   border-radius: 20px;
   background: #eee;
   font-size: 14px;
+  color: #777;
   &::placeholder {
     color: #999;
+  }
+  &.focused {
+    width: 200px;
   }
 `
 
