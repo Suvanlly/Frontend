b87242738373b4a6dcd7f6e2851f66ab3dfea988
suvanlly <swl381029812@gmail.com>
2021-09-02 09:53:54 +1000

    add loadable

--- React/iMOOC/simple_book/src/pages/detail/index.js
+++ React/iMOOC/simple_book/src/pages/detail/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { Content, DetailWrapper, Header } from './style';
 import { actionCreators } from './store';
 
@@ -28,4 +29,4 @@ const mapDispatch = (dispatch) => ({
   }
 })
 
-export default connect(mapState, mapDispatch)(Detail);
+export default connect(mapState, mapDispatch)(withRouter(Detail));
