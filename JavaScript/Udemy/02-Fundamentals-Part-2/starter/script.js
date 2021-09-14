fb937fcee8232f74bac3dfab7fd5f655e4f53860
suvanlly <swl381029812@gmail.com>
2021-09-14 21:07:34 +1000

    arrow function

--- JavaScript/Udemy/02-Fundamentals-Part-2/starter/script.js
+++ JavaScript/Udemy/02-Fundamentals-Part-2/starter/script.js
@@ -16,3 +16,31 @@ console.log(juiceProcessor(4, 0));
 
 const appleJuice = juiceProcessor(1, 5);
 console.log(appleJuice);
+
+// Function Declarations VS Expressions
+// Function Declaration
+function calcAge1(birthYear) {
+  return 2040 - birthYear;
+}
+const age1 = calcAge1(1990);
+console.log(age1);
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+  return 2040 - birthYear;
+}
+const age2 = calcAge2(1990);
+console.log(age2);
+
+
+// Arrow function
+const calcAge3 = birthYear2 => 2040 - birthYear2;
+const age3 = calcAge3(2000);
+console.log(age3); 
+
+const yearsUntilRetirement = (birthYear3, firstName) => {
+  const age = 2040 - birthYear3;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(2010, 'Jonas'));
