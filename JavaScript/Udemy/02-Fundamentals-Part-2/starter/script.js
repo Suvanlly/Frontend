a4585e96c1d06d03aac7ad56be0ebc16ba1d780b
suvanlly <swl381029812@gmail.com>
2021-09-15 19:21:11 +1000

    objects

--- JavaScript/Udemy/02-Fundamentals-Part-2/starter/script.js
+++ JavaScript/Udemy/02-Fundamentals-Part-2/starter/script.js
@@ -84,7 +84,7 @@ const newFriends = ['Michael', 'Steven', 'Peter'];
 // Add elements
 const newLength = newFriends.push('Jay');
 console.log(newFriends);
-console.log(newLength); //4
+console.log(newLength); //4, push() return the new length of array
 
 newFriends.unshift('John'); //add in the beginning of array
 console.log(newFriends);
@@ -104,3 +104,47 @@ console.log(newFriends.indexOf('Bob')); // -1
 
 console.log(newFriends.includes('Steven')); // true
 console.log(newFriends.includes('Bob')); // flase
+
+
+// Objects
+const objects = {
+  firstName: 'Jonas',
+  lastName: 'Sun',
+  age: 2030 - 1990,
+  job: 'teacher',
+  myFriend: ['Bob', 'Jack', 'Lynn']
+};
+console.log(objects);
+
+console.log(objects.job); //teacher
+console.log(objects['job']); //teacher
+
+const myJob = 'job';
+console.log(objects.myJob); 
+// undefined, because there is no property in objects with name of 'myJob'
+console.log(objects[myJob]);
+// teacher, 方括号里面可以写变量名字，不一定要写objects里面有的名字
+
+objects.location = 'Sydney'; // add property 'location' to objects
+objects['weChat'] = 'Hello'; // add property 'weChat' to objects
+console.log(objects);
+
+// Object Methods
+const newObjects = {
+  firstName: 'Sophie',
+  lastName: 'Sun',
+  birthYear: 1996,
+  calcAge4: function () {
+    console.log(this);
+    // this refer to the one who calling the calcAge method.
+    return 2021 - this.birthYear;
+  },
+  adultYears: function () {
+    this.adultYrs = this.calcAge4() - 18; // 25 - 18 = 7
+    // add new property named adultYrs.
+    return this.adultYrs;
+  }
+};
+console.log(newObjects.calcAge4()); // 25 (2021 - 1996)
+// newObjects calling the calcAge() method, so this refer to newObjects.
+console.log(newObjects.adultYears());
