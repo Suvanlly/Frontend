3253ebe1d18024abcb8beaff8f77c7d424d2f400
suvanlly <swl381029812@gmail.com>
2021-09-18 20:31:35 +1000

    refactoring code

--- JavaScript/Udemy/05-Guess my Number/starter/script.js
+++ JavaScript/Udemy/05-Guess my Number/starter/script.js
@@ -5,19 +5,22 @@ console.log(document.querySelector('.message').textContent);
 let secretNum = Math.ceil(Math.random() * 20);
 let highScore = 0;
 let score = 20; // state variable, can be used everywhere
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message;
+}
 
 // Events Listener
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  // console.log(guess);
+  // 在点击check的时候，把guess input里面输入的值转成number并赋值给guess
+  const guess = Number(document.querySelector('.guess').value); // console.log(guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🥺 No number';
+    displayMessage('🥺 No number');
 
     // When player wins
   } else if (guess === secretNum) {
-    document.querySelector('.message').textContent = '🥰 Correct Number!';
+    displayMessage('🥰 Correct Number!');
     document.querySelector('.number').textContent = secretNum;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -26,25 +29,15 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-    // When guess is too high
-  } else if (guess > secretNum) {
+
+    // When guess is wrong
+  } else if (guess !== secretNum) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '🥺 Too high';
+      displayMessage(guess > secretNum ? '🥺 Too high' : '🥺 Too low');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '🥺 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // When guess is too low
-  } else if (guess < secretNum) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '🥺 Too low';
-      score--;
-      document.querySelector('.score').textContent = score; 
-    } else {
-      document.querySelector('.message').textContent = '🥺 You lost the game!';
+      displayMessage('🥺 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
