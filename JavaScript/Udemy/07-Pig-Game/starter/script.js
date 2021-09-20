f8fb7bf2622dbce2b923f8c60a4fc065ced21024
suvanlly <swl381029812@gmail.com>
2021-09-20 13:11:08 +1000

    switching player

--- JavaScript/Udemy/07-Pig-Game/starter/script.js
+++ JavaScript/Udemy/07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const finalScore0 = document.getElementById('score--0');
 const finalScore1 = document.getElementById('score--1');
 const currentScore0 = document.getElementById('current--0');
@@ -14,7 +16,9 @@ const btnHold = document.querySelector('btn--hold');
 finalScore0.textContent = 0;
 finalScore1.textContent = 0;
 diceImg.classList.add('hidden');
+const finalScore = [0, 0];
 let score = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -28,10 +32,14 @@ btnRoll.addEventListener('click', function() {
   // 3. Check for dice number: if not 1, add to currentScore
   if (diceNum !== 1) {
     score = score + diceNum;
-    currentScore0.textContent = score;
-    //  If is 1, score is 0 and switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = score
+    
   } else {
-    score = 0;
-    currentScore0.textContent = score;
+    //  If is 1, score is 0 and switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    score = 0; //这里必须reset score，否则player2点击roll dice的时候 32行 currentScore会变成 player1的总分加上player2的
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
 })
