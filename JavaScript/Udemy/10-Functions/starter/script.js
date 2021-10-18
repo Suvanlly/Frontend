88b9836e9cf0ad76052c184e9abe4785627d6f80
suvanlly <swl381029812@gmail.com>
2021-10-18 20:01:51 +1100

    call back

--- JavaScript/Udemy/10-Functions/starter/script.js
+++ JavaScript/Udemy/10-Functions/starter/script.js
@@ -194,3 +194,21 @@ qantas.buyPlane = function () {
 
 // 如果不加 bind(qantas),this的指向会是eventListener attach的东西，也就是‘Buy new plane’这个button
 document.querySelector('.buy').addEventListener('click', qantas.buyPlane.bind(qantas));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
