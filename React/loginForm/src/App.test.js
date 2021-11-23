1c1b56d5cdbaecce0221d44236617c50bf5412b7
suvanlly <swl381029812@gmail.com>
2021-11-23 14:44:02 +1100

    edit files

--- React/loginForm/src/App.test.js
+++ React/loginForm/src/App.test.js
@@ -1,3 +1,10 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import App, { validateInput } from "./App";
+
+test("login form should be in the document", () => {
+  const component = render(<App />);
+  // console.log(component)
+  const inputNode = component.getByText("Email:");
+  expect(inputNode).toBeInTheDocument();
+});
