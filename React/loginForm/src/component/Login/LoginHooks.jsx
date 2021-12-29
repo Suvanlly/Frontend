4fa671f375bf37708e8587f74e16334cb902ed53
suvanlly <swl381029812@gmail.com>
2021-12-29 12:49:35 +1100

    edit

--- React/loginForm/src/component/Login/LoginHooks.jsx
+++ React/loginForm/src/component/Login/LoginHooks.jsx
@@ -3,24 +3,15 @@ import axios from "axios";
 import { Input, InputWrap, Button, Label } from "./styles/login";
 
 const LoginHooks = () => {
-  
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
-  usernameInputHandler(e) {
-    const inputUsername = e.target.value;
-    // console.log(e.target.value);
-    this.setState({
-      username: inputUsername,
-    });
-  }
+  const { username, password } = formData;
 
-  passwordInputHandler(e) {
-    const inputPassword = e.target.value;
-    this.setState({
-      password: inputPassword,
-    });
-
-    // setInterval
-  }
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
   submitHandler = (event) => {
     // event.preventDeafault(): prevent submiting the form until formValue assigned
@@ -43,36 +34,34 @@ const LoginHooks = () => {
       });
   };
 
-  render() {
-    return (
-      <InputWrap>
-        <form data-testid="login-form" onSubmit={this.submitHandler}>
-          <Label htmlFor="username">Username:</Label>
-          <Input
-            type="text"
-            id="username"
-            data-testid="username"
-            // make input username value appear on the input box
-            value={this.state.username}
-            // whenever user type something inside input box, onChange event will be executed
-            onChange={this.usernameInputHandler}
-          />
-          <Label htmlFor="password">Password:</Label>
-          <Input
-            type="password"
-            id="password"
-            data-testid="password"
-            value={this.state.password}
-            onChange={this.passwordInputHandler}
-          />
-          <Button type="submit">Submit</Button>
-          <Button>
-            <a href="/">Cancel</a>
-          </Button>
-        </form>
-      </InputWrap>
-    );
-  }
-}
+  return (
+    <InputWrap>
+      <form data-testid="login-form" onSubmit={this.submitHandler}>
+        <Label htmlFor="username">Username:</Label>
+        <Input
+          type="text"
+          id="username"
+          data-testid="username"
+          // make input username value appear on the input box
+          value={username}
+          // whenever user type something inside input box, onChange event will be executed
+          onChange={onChange}
+        />
+        <Label htmlFor="password">Password:</Label>
+        <Input
+          type="password"
+          id="password"
+          data-testid="password"
+          value={password}
+          onChange={onChange}
+        />
+        <Button type="submit">Submit</Button>
+        <Button>
+          <a href="/">Cancel</a>
+        </Button>
+      </form>
+    </InputWrap>
+  );
+};
 
 export default LoginHooks;
