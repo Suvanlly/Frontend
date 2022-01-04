adcc3d29a55e8167e7581b9714e719281a6f0731
suvanlly <swl381029812@gmail.com>
2022-01-04 12:42:51 +1100

    edit code

--- React/react-hooks/src/useEffect/UseEffectDemo.js
+++ React/react-hooks/src/useEffect/UseEffectDemo.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function UseEffectDemo() {
   const [resourceType, setResourceType] = useState("posts");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  // This empty array lead to onMount because it only happens the first time you render it, it won't change later
+  useEffect(() => {
+    console.log("onMount");
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
 
   return (
     <>
+      <h2>The size of the window is: {windowWidth}</h2>
       <div>
         <button onClick={() => setResourceType("posts")}>Posts</button>
         <button onClick={() => setResourceType("users")}>Users</button>
