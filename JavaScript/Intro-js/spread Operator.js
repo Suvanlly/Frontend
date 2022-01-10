f1a188316162dec924a892cc42015bc34e6d90cc
suvanlly <swl381029812@gmail.com>
2022-01-10 22:26:35 +1100

    edit code

---
+++ JavaScript/Intro-js/spread Operator.js
@@ -0,0 +1,16 @@
+// ES6 provides a new operator called spread operator that consists of three dots (...).
+// The spread operator allows you to spread out elements of an iterable object such as an array,a  map, or a set.
+// For example:
+const odd = [1, 3, 5];
+const combined = [2, 4, 6, ...odd];
+console.log(combined);
+// Output: [2,4,6,1,3,5]
+
+// Note that ES6 also has the three dots ( ...) which is a rest parameter
+// that collects all remaining arguments of a function into an array.
+function f(a, b, ...args) {
+  console.log(args);
+}
+f(1, 2, 3, 4, 5);
+// output: [3,4,5]
+// The rest parameters must be the last arguments of a function. However, the spread operator can be anywhere
