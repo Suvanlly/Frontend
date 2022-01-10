3e5aeea93b379477eef2efaa3f8967fca7a6b82d
suvanlly <swl381029812@gmail.com>
2022-01-11 10:36:25 +1100

    edit code

--- JavaScript/Intro-js/spread Operator.js
+++ JavaScript/Intro-js/spread Operator.js
@@ -13,4 +13,21 @@ function f(a, b, ...args) {
 }
 f(1, 2, 3, 4, 5);
 // output: [3,4,5]
-// The rest parameters must be the last arguments of a function. However, the spread operator can be anywhere
+// The rest parameters must be the last arguments of a function.
+
+// However, the spread operator can be anywhere
+const odd = [1, 3, 5];
+const combined = [2, ...odd, 4, 6];
+console.log(combined);
+// Output:[2,1,3,5,4,6]
+
+// Concatenating array
+let numbers = [1, 2];
+let moreNumbers = [3, 4];
+let allNumbers = [...numbers, ...moreNumbers];
+console.log(allNumbers); // [1, 2, 3, 4]
+
+// Copying an array
+let scores = [80, 70, 90];
+let copiedScores = [...scores];
+console.log(copiedScores); // [80, 70, 90]
