9d1aba6ec7ec67642cfecce69833c2ee9a6712be
suvanlly <swl381029812@gmail.com>
2022-01-12 15:58:49 +1100

    edit code

--- JavaScript/Intro-js/Const, Let and Var.js
+++ JavaScript/Intro-js/Const, Let and Var.js
@@ -13,6 +13,8 @@ f();
 console.log(a);
 // 10 10
 
+// ------------------------------------------------------------------------------------------------------
+
 // Example 2: Variable 'a' is declared inside the function.
 function f() {
   // It can be accessible anywhere within this function
@@ -24,3 +26,44 @@ f();
 console.log(a);
 // 10
 // ReferenceError: a is not defined
+
+// ----------------------------------------------------------------------------------------------------
+
+// Example 3: User can re-declare variable and user can update var variable.
+var a = 10;
+
+var a = 8; // user can re-declare variable
+
+a = 7; // user can update var variable
+console.log(a); // 7
+
+// ----------------------------------------------------------------------------------------------------
+
+// Example 4: var will be initialized with undefined if user use it before declaration
+console.log(a); // undefined
+
+var a = 10;
+
+// ----------------------------------------------------------------------------------------------------
+
+/* Let
+Scope: Block scoped.
+It can't be accessible outside the particular block.
+*/
+
+// Example 1: accessing the let variable outside the function block
+let a = 10;
+function f() {
+  if (true) {
+    let b = 9;
+    console.log(b);
+  }
+  console.log(b);
+}
+f();
+
+console.log(a);
+
+// ----------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------
