a864d7363077a891b3021a067f56bb35b3a854a9
suvanlly <swl381029812@gmail.com>
2022-01-17 12:19:20 +1100

    edit code

---
+++ JavaScript/Intro-js/Undefined and Undeclared.js
@@ -0,0 +1,7 @@
+//  Undefined: a variable has been declared but has not been initialized with a value
+let message;
+console.log(message); //undefined
+
+// undeclared is a variable has not been declared.
+console.log(counter);
+// ReferenceError: counter is not defined
