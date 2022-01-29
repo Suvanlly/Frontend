d722c0f152e0f063f214f6fb70a570a1f5d12b39
suvanlly <swl381029812@gmail.com>
2022-01-29 22:31:52 +1100

    edit file

---
+++ React/Interview/Basic/function vs class component.jsx
@@ -0,0 +1,9 @@
+// Class component: are declared using ES6 class
+class card extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>123</div>;
+  }
+}
