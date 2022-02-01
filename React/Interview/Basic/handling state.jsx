3eb0182ddfbb05266a7b1289c72cf90c80d96faa
suvanlly <swl381029812@gmail.com>
2022-02-01 19:27:14 +1100

    edit file

--- React/Interview/Basic/handling state.jsx
+++ React/Interview/Basic/handling state.jsx
@@ -1,4 +1,5 @@
-// Function components use React hooks to handle state, It uses the useState hook to set the state of a variable inside the component
+// Function components use React hooks to handle state
+// It uses the useState hook to set the state of a variable inside the component
 function ClassRoom(props) {
   let [studentCount, setStudentCount] = useState(0);
   const addStudent = () => {
@@ -12,3 +13,30 @@ function ClassRoom(props) {
     </div>
   );
 }
+
+// Convert it into a class component
+
+class ClassRoom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { studentCount: 0 };
+    this.addStudent = this.addStudent.bind(this);
+  }
+  addStudent() {
+    this.setState((prevState) => {
+      return {
+        studentCount: prevState.studentCount++,
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <P>Number of Students: {this.state.studentCount}</P>
+        <button onClick={this.addStudent}>Add Student</button>
+      </div>
+    );
+  }
+}
+
+export default ClassRoom;
