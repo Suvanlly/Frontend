eebe3ae7b7ee4d61f0bd983400af9128f76bdf09
suvanlly <swl381029812@gmail.com>
2022-02-07 11:10:33 +1100

    edit file

--- JavaScript/Intro-js/debounce and throttle.js
+++ JavaScript/Intro-js/debounce and throttle.js
@@ -21,8 +21,33 @@ input1.addEventListener('keyup', debounce(function() {
 }, 600))
 
 /*
+绑定事件解释：addEventListener 第一个参数是监听的事件，第二个参数是对应事件的回调函数。将 debounce 函数作为回调函数，
+这个 debounce 回调函数返回一个防抖之后的函数，因此实现了防抖的功能。
 
+防抖解释：当按下某个键的时候触发 keydown 事件，并执行回调。timer 默认为 null，在 return 的函数中定时器 timer 被赋值，
+如果在 delay 延迟之内再次触发了 keydown 事件，那么 timer 就会被重置为null...，当用户输入完成之后（delay 时间已过），
+那么就会触发 debounce 中的回调函数，也就是 keydown 最终要执行的事件。
+
+函数节流的目的: 是为了限制函数一段时间内只能执行一次。因此，通过使用定时任务，延时方法执行。
+在延时的时间内，方法若被触发，则直接退出方法。从而实现一段时间内只执行一次。
 */
 
 
 // Throttle 函数节流，限制一个函数在一定时间内只能执行一次
+function throttle(fn, delay) {
+  let timer = null
+  return function() {
+    if (timer) {
+      return
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, arguments)
+      timer = null
+    })
+  }
+}
+// test
+let div1 = document.getElementById('div')
+div1.addEventListener('drag', throttle(function(e) {
+  console.log(e.offsetX, e.offsetY)
+}, 100))
